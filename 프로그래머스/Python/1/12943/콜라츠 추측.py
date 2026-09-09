5f11ff7c7f2563ffa6df8f865ecbf8ddf59ca4dc
Jeong SuHwan <102617792+MMintchocolate@users.noreply.github.com>
2026-09-10 02:01:20 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/Python/1/12943/콜라츠 추측.py
@@ -0,0 +1,16 @@
+def solution(num):
+    answer = 0
+    if num == 1:
+        return 0
+    while True:
+        answer+=1
+        if answer >  500:
+            answer = -1
+            break
+        if num%2 == 0:
+            num//=2
+        else:
+            num=(num*3) + 1
+        if num == 1:
+            break
+    return answer
